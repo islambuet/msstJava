77e93008003b60c8d2c30816a914557b0c83a024
Md. Shaiful Islam <islambuet@gmail.com>
2024-02-03 14:28:17 +0600

    Initialized Java

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# msstJava
+# Copy Code From Existing Project
+# rename Module
+# rename project
+    open main.java
+    select the package name and rename it
+    Make sure porm.xml copied for external libray and .idea aswell
+# Create Artifacts for exe 
+# for database timer
+    SET GLOBAL event_scheduler = ON;
---
+++ src/main/java/aclusterllc/msst/Test.java
@@ -0,0 +1,22 @@
+package aclusterllc.msst;
+
+import org.json.JSONObject;
+
+import java.sql.*;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+import static java.lang.String.format;
+//import java.sql.Connection;
+//import java.sql.ResultSet;
+//import java.sql.Statement;
+//import java.sql.ResultSetMetaData;
+
+
+public class Test {
+    public static void main(String[] args)
+    {
+        System.out.println("test started");
+    }
+
+}
